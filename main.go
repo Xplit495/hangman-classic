392cd8c5ae6776a4b0e579726405ad7e9b3ee21a
carquentin <quentin.carrola@ynov.com>
2023-11-27 14:36:49 +0100

    FIX USER INPUT WORDs

--- main.go
+++ main.go
@@ -15,15 +15,18 @@ var yellow = "\033[33m"
 var red = "\033[31m"
 var green  = "\033[32m"
 var reset  = "\033[0m"
+
 var liveJose = 10
+var choiceToLowerRune []rune
 var letterHistory []string
 var letterHistoryEnd []string
+var wordHistory []string
 var currentDir, _ = os.Getwd()
-var choiceToLowerRune []rune
-var choiceToLowerStrings  []string
+
 
 func main() {
-	chooseDifficulty()
+	ClearTerminal()
+	rules()
 }
 
 func ClearTerminal() {
@@ -35,6 +38,16 @@ func ClearTerminal() {
 	}
 	cmd.Stdout = os.Stdout
 	cmd.Run()
+}
+
+func rules() {
+	fmt.Println("")
+	fmt.Println("Bienvenue dans ce super jeu, les régles sont simples :")
+	fmt.Println("- Vous pouvez proposer ou un mot ou une lettre")
+	fmt.Println("- Une mauvaise lettre vous retire" + yellow + " une " + reset + "vie. Mais " + red + "attention" + reset + " car un mauvais mot vous en retire" + yellow + " 2" + reset + " !")
+	fmt.Print("Appuyer sur entrer pour continuer : ")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	chooseDifficulty()
 }
 
 func chooseDifficulty(){
@@ -53,18 +66,6 @@ var difficulty int
 	selectDictionnary(difficulty)
 }
 
-func selectDictionnaryPath(absolutePath string){
-	f, _ := os.Open(absolutePath)
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
-	var wordList []string
-	for scanner.Scan() {
-		wordList = append(wordList, scanner.Text())
-	}
-	randomWord(wordList)
-}
-
 func selectDictionnary(difficulty int){
 	switch difficulty {
 	case 1:
@@ -80,6 +81,18 @@ func selectDictionnary(difficulty int){
 		absolutePath := currentDir + "\\" + relativePath
 		selectDictionnaryPath(absolutePath)
 	}
+}
+
+func selectDictionnaryPath(absolutePath string){
+	f, _ := os.Open(absolutePath)
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+	var wordList []string
+	for scanner.Scan() {
+		wordList = append(wordList, scanner.Text())
+	}
+	randomWord(wordList)
 }
 
 func randomWord(wordList []string){
@@ -140,95 +153,153 @@ func associateClueToWord(randomClues []int, arrSelectWord []string){
 	}
 
 	fmt.Println("")
-
 	fmt.Print("\nLe mot avec le(s) indice(s) est : ")
 	printWordPartiallyReveal(wordPartiallyReveal)
 	fmt.Println("")
 	startGame(arrSelectWord,wordPartiallyReveal,10)
 }
 
+
 func startGame(arrSelectWord []string, wordPartiallyReveal [] string, liveJose int) {
+	var choiceToLowerStrings  []string
 	printLive(liveJose)
 	fmt.Println("")
 	fmt.Printf("Il vous reste "+yellow+"%d vie "+reset+"avant d'être pendu !\n", liveJose)
+	fmt.Println("")
 	fmt.Print("Les lettres déjà essayé sont : ")
 	printLetterHistoryInGame()
-	var choice string
+	fmt.Print("Les mots déjà essayé sont : ")
+	printWordHistory()
+
 	var choiceToLower string
+	var choice string
 	for i := 0; i <= 1; i++ {
-		fmt.Print("Entrez votre lettre : ")
+		fmt.Print("Entrez votre lettre ou votre mot : ")
 		fmt.Scan(&choice)
 		choiceToLower = strings.ToLower(choice)
 		choiceToLowerRune = []rune(choiceToLower)
-		for j := 0; j < len(choiceToLowerRune); j++ {
-			choiceToLowerStrings = append(choiceToLowerStrings, string(choiceToLowerRune[j]))
-		}
-		exit := true
-		for k := 0; k < len(choiceToLowerRune); k++ {
-			if choiceToLowerRune[k] >= rune(97) && choiceToLowerRune[k] <= rune(122) {
-				if k+1 == len(choiceToLowerRune) {
-					break
-				}
-			}else{
-				ClearTerminal()
-				fmt.Println("Merci de saisir" + red + " UNIQUEMENT " + reset + "des caractère de l'alphabet !")
-				exit = false
-				i--
+		if len(wordPartiallyReveal) == len(choiceToLowerRune) || len(choiceToLowerRune) == 1{
+			for j := 0; j < len(choiceToLowerRune); j++ {
+				choiceToLowerStrings = append(choiceToLowerStrings, string(choiceToLowerRune[j]))
+			}
+			exit := true
+			for k := 0; k < len(choiceToLowerRune); k++ {
+				if choiceToLowerRune[k] >= rune(97) && choiceToLowerRune[k] <= rune(122) {
+					if k+1 == len(choiceToLowerRune) {
+						break
+					}
+				}else{
+					ClearTerminal()
+					fmt.Println("Merci de saisir" + red + " UNIQUEMENT " + reset + "des caractère de l'alphabet !")
+					exit = false
+					i--
 				}
 			}
 			if exit == true {
 				break
 			}
+		}else{
+			ClearTerminal()
+			fmt.Println("Merci de saisir " + red + "UNIQUEMENT " + reset + "une lettre ou un mot !")
+			i--
 		}
-		letterHistory = append(letterHistory, choiceToLower)
-		letterHistoryEnd = append(letterHistoryEnd, choiceToLower)
-		refreshWord(arrSelectWord,choiceToLower,wordPartiallyReveal,choiceToLowerStrings)
+	}
+	if len(choiceToLowerStrings) == 1 {
+		for i := 0; i < len(choiceToLowerStrings); i++ {
+			letterHistory = append(letterHistory, choiceToLowerStrings[i])
+			letterHistoryEnd = append(letterHistoryEnd, choiceToLowerStrings[i])
+		}
+	}else{
+		wordHistory = append(wordHistory, choiceToLower)
+	}
+	refreshWord(arrSelectWord, wordPartiallyReveal, choiceToLowerStrings)
 }
 
-func refreshWord(arrSelectWord []string, choiceToLower string, wordPartiallyReveal []string,choiceToLowerStrings []string) {
-	//letterFind := false
-	for index, letter := range arrSelectWord {
-		for i := 0; i < len(choiceToLowerStrings); i++ {
-			if letter == choiceToLowerStrings[i] {
+func refreshWord(arrSelectWord []string, wordPartiallyReveal []string, choiceToLowerStrings []string) {
+	letterFind := false
+	wordFind := false
+	if len(choiceToLowerStrings) == 1 {
+		for index, letter := range arrSelectWord {
+			if letter == choiceToLowerStrings[0] {
 				wordPartiallyReveal[index] = letter
-				//letterFind = true
+				letterFind = true
 			}
 		}
-
+	} else {
+		counter := 0
+		for i := 0; i < len(arrSelectWord); i++ {
+			if arrSelectWord[i] == choiceToLowerStrings[i] {
+				counter++
+			}
+		}
+		if counter == len(arrSelectWord) {
+			wordPartiallyReveal = choiceToLowerStrings
+			wordFind = true
+		}
 	}
 	printWordPartiallyReveal(wordPartiallyReveal)
-	//histroy(choiceToLower, letterFind ,wordPartiallyReveal,arrSelectWord)
+	histroy(choiceToLowerStrings, letterFind, wordFind,wordPartiallyReveal,arrSelectWord)
 }
 
-func histroy (choiceToLower string, letterFind bool, wordPartiallyReveal []string, arrSelectWord []string){
+func histroy (choiceToLowerStrings []string, letterFind bool, wordFind bool,wordPartiallyReveal []string, arrSelectWord []string){
 	counter := 0
 	letterAlreadyUse := false
-	for _, char := range letterHistory {
-		if choiceToLower == char {
-			counter++
-			if counter > 1 {
-				counter = 0
-				letterAlreadyUse = true
-				if len(letterHistory) > 0 {
-					letterHistory = letterHistory[:len(letterHistory)-1]
+	wordAlreadyUse := false
+	if len(choiceToLowerStrings) == 1 {
+		for _, char := range letterHistory {
+			for i := 0; i < len(choiceToLowerStrings); i++ {
+				if choiceToLowerStrings[i] == char {
+					counter++
+					if counter > 1 {
+						counter = 0
+						letterAlreadyUse = true
+						if len(letterHistory) > 0 {
+							letterHistory = letterHistory[:len(letterHistory)-1]
+						}
+					}
+				}
+			}
+		}
+	}else{
+		var wordTry string
+		for i := 0; i < len(choiceToLowerStrings); i++ {
+			wordTry = wordTry + choiceToLowerStrings[i]
+		}
+		if len(wordHistory) > 1 {
+			for i := 0; i < len(wordHistory); i++ {
+				if wordHistory[i] == wordTry {
+					counter++
+					if counter > 1 {
+						counter = 0
+						wordAlreadyUse = true
+						if len(wordHistory) > 0 {
+							wordHistory = wordHistory[:len(wordHistory)-1]
+						}
+					}
 				}
 			}
 		}
 	}
-	checkLetterUses(letterFind, letterAlreadyUse, wordPartiallyReveal,arrSelectWord)
+	checkElementUses(choiceToLowerStrings,letterFind, wordFind, letterAlreadyUse, wordPartiallyReveal,arrSelectWord, wordAlreadyUse)
 }
 
-func checkLetterUses(letterFind bool, alreadyUse bool, wordPartiallyReveal []string, arrSelectWord []string) {
-	if alreadyUse == true {
+func checkElementUses(choiceToLowerStrings [] string,letterFind bool, wordFind bool, letterAlreadyUse bool, wordPartiallyReveal []string, arrSelectWord []string, wordAlreadyUse bool){
+	if letterAlreadyUse == true {
 			ClearTerminal()
-			fmt.Println(red+"Vous avez déjà essayé cette lettre !"+reset) //Pas fonctionnel mais à laisser pour les advandec features !!!
+			fmt.Println(red+"Vous avez déjà essayé cette lettre, attention !"+reset)
 			startGame(arrSelectWord,wordPartiallyReveal,liveJose)
+	}else if wordAlreadyUse == true {
+		ClearTerminal()
+		fmt.Println(red+"Vous avez déjà essayé ce mot, attention !"+reset)
+		startGame(arrSelectWord,wordPartiallyReveal,liveJose)
 	}
 	fmt.Println("")
-	findLetter(wordPartiallyReveal,arrSelectWord,letterFind)
+	findLetter(choiceToLowerStrings,wordPartiallyReveal,arrSelectWord,letterFind,wordFind)
 }
 
-func findLetter(wordPartiallyReveal []string,arrSelectWord []string, letterFind bool){
+
+func findLetter(choiceToLowerStrings [] string,wordPartiallyReveal []string,arrSelectWord []string, letterFind bool, wordFind bool){
+	if len(choiceToLowerStrings) == 1 {
 	if letterFind == true{
 		ClearTerminal()
 		fmt.Println(green+"Bonne lettre !"+reset)
@@ -236,14 +307,29 @@ func findLetter(wordPartiallyReveal []string,arrSelectWord []string, letterFind 
 		fmt.Printf("Pour le moment le mot ressemble à ca -> ")
 		printWordPartiallyReveal(wordPartiallyReveal)
 		checkWordFind(wordPartiallyReveal,arrSelectWord)
-	}else{
+	}else if letterFind == false {
 		liveJose--
 		ClearTerminal()
-		fmt.Println(red+"Mauvaise lettre !"+reset)
+		fmt.Println(red + "Mauvaise lettre !" + reset)
 		fmt.Println("")
 		fmt.Printf("Pour le moment le mot ressemble à ca -> ")
 		printWordPartiallyReveal(wordPartiallyReveal)
-		checkWordFind(wordPartiallyReveal,arrSelectWord)
+		checkWordFind(wordPartiallyReveal, arrSelectWord)
+	}
+	}else{
+		if wordFind == false{
+			liveJose = liveJose - 2
+			ClearTerminal()
+			fmt.Println(red+"Mauvais mot !"+reset)
+			fmt.Println("")
+			fmt.Printf("Pour le moment le mot ressemble à ca -> ")
+			printWordPartiallyReveal(wordPartiallyReveal)
+			checkWordFind(wordPartiallyReveal,arrSelectWord)
+		}else if wordFind == true{
+			ClearTerminal()
+			printWordPartiallyReveal(wordPartiallyReveal)
+			checkWordFind(wordPartiallyReveal,arrSelectWord)
+		}
 	}
 }
 
@@ -257,21 +343,25 @@ func checkWordFind(wordPartiallyReveal []string,arrSelectWord []string) {
 	}
 	if wordFind == true {
 		ClearTerminal()
-		fmt.Println("\nVous avez deviné le mot !")
-		fmt.Print("Vos propositions ont étaient : ")
+		fmt.Println("\n"+ green + "Vous avez deviné le mot !"+ reset)
+		fmt.Print("Vous avez essayé les lettres suivantes : ")
 		printLetterHistoryEnd()
+		fmt.Print("Vous avez essayé les mots suivants : ")
+		printWordHistory()
 		fmt.Print("Le mot était : ")
 		printWord(arrSelectWord)
-	}else if liveJose == 0{
+	}else if liveJose <= 0{
 		ClearTerminal()
-		fmt.Print("\nVous n'avez plus de vie. Le mot était : ")
+		fmt.Print("\n"+ red+"Vous n'avez plus de vie !"+reset + "Le mot était : ")
 		printWord(arrSelectWord)
 		fmt.Println("")
 		fmt.Println("")
 		printJose(71,78)
-		fmt.Print("Vos propositions ont étaient : ")
+		fmt.Print("Vous avez essayé les lettres suivantes : ")
 		printLetterHistoryEnd()
-		fmt.Println("Vous êtes pendu !")
+		fmt.Print("Vous avez essayé les mots suivants : ")
+		printWordHistory()
+		fmt.Println(red+"Vous êtes pendu !"+reset)
 	}else{
 		startGame(arrSelectWord,wordPartiallyReveal,liveJose)
 	}
@@ -295,6 +385,13 @@ func printLetterHistoryInGame(){
 		fmt.Print(" ")
 	}
 	fmt.Println("")
+}
+
+func printWordHistory(){
+	for i := 0; i < len(wordHistory); i++ {
+	fmt.Print(wordHistory[i])
+		fmt.Print(" ")
+	}
 	fmt.Println("")
 }
 																		//A séparer dans des dossiers
@@ -313,9 +410,6 @@ func printWord(arrSelectWord []string){
 	}
 }
 //Fin des fonctions d'affichage
-
-
-
 
 
 
