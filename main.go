fc1811741f515d51d80a025735ecca819c454204
carquentin <quentin.carrola@ynov.com>
2023-11-27 14:11:22 +0100

    fix clues bug

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 	"runtime"
 	"sort"
-	"strings"
 )
 
 func main() {
@@ -28,105 +27,139 @@ func ClearTerminal() {
 	cmd.Run()
 }
 
-func chooseDifficulty() {
-	var difficulty int
+//LiveJose a la fin du code !!!!
+
+func chooseDifficulty(){
+var difficulty int // CHOISIR NIVEAU DIFFICULTE (POUR LE MOMENT UTILISER SEULEMENT DIIFUCLTE 1 ON CODERA LES AUTRES PLUS TARD)
 	for i := 0; i <= 1; i++ {
 		ClearTerminal()
 		fmt.Println("")
-		fmt.Print("Choose your level of difficulty (1-3), 1: Easy, 2: Medium, 3: Hard. What you choose : ")
+		fmt.Print("Choose your level of difficulty (1-3), 1: Easy, 2: Medium, 3: Hard. What you choose : ") // CHOISIR NIVEAU DIFFICULTE (POUR LE MOMENT UTILISER SEULEMENT DIIFUCLTE 1 ON CODERA LES AUTRES PLUS TARD)
 		fmt.Scanln(&difficulty)
-		if difficulty == 1 || difficulty == 2 || difficulty == 3 {
+		if difficulty != 1 && difficulty != 2 && difficulty != 3 {
+			i--
+		} else {
 			break
-		}
+		} // CHOISIR NIVEAU DIFFICULTE (POUR LE MOMENT UTILISER SEULEMENT DIIFUCLTE 1 ON CODERA LES AUTRES PLUS TARD)
 	}
-
-	var filename string
-
-	switch difficulty {
-	case 1:
-		filename = "C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words.txt"
-	case 2:
-		filename = "C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words2.txt"
-	case 3:
-		filename = "C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words3.txt"
-	}
-
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Erreur lors de l'ouverture du fichier:", err)
-		return
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
-	wordlist := []string{}
-	for scanner.Scan() {
-		wordlist = append(wordlist, scanner.Text())
-	}
-
-	randomWord := rand.Intn(len(wordlist))
-	if randomWord < 0 {
-		randomWord = 0
-	}
-	selectWord := wordlist[randomWord]
-
-	hangman(selectWord)
+	selectDictionnary(difficulty)
 }
 
-func hangman(selectWord string) {
+func selectDictionnary(difficulty int){
+	switch difficulty {
+	case 1:
+		f, _ := os.Open("C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words.txt")
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanWords)
+		wordList := []string{}
+		for scanner.Scan() {
+			wordList = append(wordList, scanner.Text())
+		}
+		randomWord(wordList)
+	case 2:
+		f, _ := os.Open("C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words2.txt")
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanWords)
+		wordList := []string{}
+		for scanner.Scan() {
+			wordList = append(wordList, scanner.Text())
+		}
+		randomWord(wordList)
+	case 3:
+		f, _ := os.Open("C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words3.txt")
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanWords)
+		wordList := []string{}
+		for scanner.Scan() {
+			wordList = append(wordList, scanner.Text())
+		}
+		randomWord(wordList)
+	}
+
+}
+
+func randomWord(wordList []string){
+	indexRandomWord := rand.Intn(len(wordList)) - 1 // SELECTIONNE UN MOT AU HASARD DANS LE DICTIONNAIRE ET LE MET DANS UN TABLEAU
+	if indexRandomWord <= 0 {
+		indexRandomWord += 1
+	}
+	selectWord := wordList[indexRandomWord]
 	var arrSelectWord []string
-	liveJose := 0
-	live := []string{"", "=========\n", "    |  \n    |  \n    |  \n    |  \n    |  \n=========\n", "  +---+  \n      |  \n      |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n      |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n  |   |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|   |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n /    |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n / \\  |  \n      |  \n=========\n"}
-	n := (len(selectWord) / 2) - 1 //LIS LA CONSIGNE POUR COMPRENDRE
-	var randomIndexLetter []int
-	usedIndices := make(map[int]bool)
+	for i := 0; i < len(selectWord); i++ {
+		arrSelectWord = append(arrSelectWord, string(selectWord[i]))
+	} // SELECTIONNE UN MOT AU HASARD DANS LE DICTIONNAIRE ET LE MET DANS UN TABLEAU
+	fmt.Println(arrSelectWord)
+	findWordClue(arrSelectWord)
+}
 
+func findWordClue(arrSelectWord []string){
+	n := (len(arrSelectWord) / 2) - 1 //LIS LA CONSIGNE POUR COMPRENDRE
+	var randomClues []int
+
+	usedClues := make(map[int]bool)
 	for i := 1; i <= n; i++ {
-		var newIndex int
+		var newClue int
 		for {
-			newIndex = rand.Intn(len(arrSelectWord) - 1)
-
-			if !usedIndices[newIndex] {
-				usedIndices[newIndex] = true
+			newClue = rand.Intn(len(arrSelectWord) - 1)
+			if !usedClues[newClue] {
+				usedClues[newClue] = true
 				break
 			}
 		}
-		randomIndexLetter = append(randomIndexLetter, newIndex)
+		randomClues = append(randomClues, newClue)
 	}
+	sort.Ints(randomClues)
+	fmt.Print(randomClues)
+	associateClueToWord(randomClues, arrSelectWord)
+}
 
-	sort.Ints(randomIndexLetter)
-
+func associateClueToWord(randomClues []int, arrSelectWord []string){
 	values := 0 // SERT A AFFICHER SEULEMENT LES LETTRES ALEATOIRES CHOISIS PRECEDEMENT
 	var wordPatriallyReveal []string
-	if len(randomIndexLetter) == 0 {
+	if len(randomClues) == 0 {
 		for i := 0; i <= len(arrSelectWord)-1; i++ {
 			wordPatriallyReveal = append(wordPatriallyReveal, "_")
 		}
 	} else {
 		for i := 0; i <= len(arrSelectWord)-1; i++ {
-			if i == randomIndexLetter[values] { // SERT A AFFICHER SEULEMENT LES LETTRES ALEATOIRES CHOISIS PRECEDEMENT
+			if i == randomClues[values] { // SERT A AFFICHER SEULEMENT LES LETTRES ALEATOIRES CHOISIS PRECEDEMENT
 				wordPatriallyReveal = append(wordPatriallyReveal, arrSelectWord[i])
-				if values+1 >= len(randomIndexLetter) {
+				if values+1 >= len(randomClues) {
 					values = 0
 				} else {
 					values += 1
 				}
-			} else {
+			}else {
 				wordPatriallyReveal = append(wordPatriallyReveal, "_")
 			} // SERT A AFFICHER SEULEMENT LES LETTRES ALEATOIRES CHOISIS PRECEDEMENT
 		}
 	}
 
+	fmt.Println("")
 	fmt.Print("\nLe mot avec le(s) indice(s) est : ")
 	for i := 0; i < len(wordPatriallyReveal); i++ {
 		fmt.Print(wordPatriallyReveal[i])
 	}
 	fmt.Println("")
 	fmt.Println("")
+}
 
-	tryRemain := 10 // Nombre d'essais max
+func startGame(){
+	//Commence à partir d'ici (tu peux tester le reste mais normallement tout fonctionne regarde comment j'ai fais et tu continues pour le reste vasy bonne chance je viens a 15h.)
+	// ET SURTOUT MODIFIE PAS MON CODE AU DESSUS A MOINS QUE CA SOIT VRAIMENT OBLIGATOIRE !!!
+}
 
+
+/*
+Fais juste des copier coller du code en dessous.
+
+liveJose := 0
+live := []string{"", "=========\n", "    |  \n    |  \n    |  \n    |  \n    |  \n=========\n", "  +---+  \n      |  \n      |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n      |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n      |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n  |   |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|   |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n      |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n /    |  \n      |  \n=========\n", "  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n / \\  |  \n      |  \n=========\n"}
+
+	tryRemain := 10 // Nombre d'essais maximum
 	var letterHistory []string
 	var letterHistoryEnd []string
 	for tryRemain > 0 {
@@ -177,138 +210,89 @@ func hangman(selectWord string) {
 			}
 		}
 
-		if letterFind == true {
-			ClearTerminal()
-			if alreadyUse == true {
-				fmt.Println("VOUS AVEZ DEJA ESSAYEZ CETTE LETTRE ! FAITES ATTENTION.")
-			}
-			fmt.Println("")
-			fmt.Println("Bonne lettre !")
-			fmt.Println("")
-			fmt.Printf("Pour le moment le mot ressemble à ca -> ")
-			for i := 0; i < len(wordPatriallyReveal); i++ {
-				fmt.Print(wordPatriallyReveal[i])
-			}
-			fmt.Println("")
-			fmt.Print("Les lettres déjà essayé sont : ")
-			for i := 0; i <= len(letterHistory)-1; i++ {
-				fmt.Print(letterHistory[i])
-				fmt.Print(" ")
-			}
-			fmt.Println("")
-			fmt.Println("")
+			if letterFind == true {
+				ClearTerminal()
+				if alreadyUse == true {
+					fmt.Println("VOUS AVEZ DEJA ESSAYEZ CETTE LETTRE ! FAITES ATTENTION.")
+				}
+				fmt.Println("")
+				fmt.Println("Bonne lettre !")
+				fmt.Println("")
+				fmt.Printf("Pour le moment le mot ressemble à ca -> ")
+				for i := 0; i < len(wordPatriallyReveal); i++ {
+					fmt.Print(wordPatriallyReveal[i])
+				}
+				fmt.Println("")
+				fmt.Print("Les lettres déjà essayé sont : ")
+				for i := 0; i <= len(letterHistory)-1; i++ {
+					fmt.Print(letterHistory[i])
+					fmt.Print(" ")
+				}
+				fmt.Println("")
+				fmt.Println("")
 
-		} else {
-			ClearTerminal()
-			if alreadyUse == true {
-				fmt.Println("VOUS AVEZ DÉJA ESSAYEZ CETTE LETTRE ! FAITES ATTENTION.")
+			} else {
+				ClearTerminal()
+				if alreadyUse == true {
+					fmt.Println("VOUS AVEZ DÉJA ESSAYEZ CETTE LETTRE ! FAITES ATTENTION.")
+				}
+				tryRemain--
+				liveJose++
+				fmt.Println("")
+				fmt.Println("Mauvaise lettre.")
+				fmt.Println("")
+				fmt.Printf("Pour le moment le mot ressemble à ca -> ")
+				for i := 0; i < len(wordPatriallyReveal); i++ {
+					fmt.Print(wordPatriallyReveal[i])
+				}
+				fmt.Println("")
+				fmt.Print("L'historique de vos lettres est : ")
+				for i := 0; i <= len(letterHistory)-1; i++ {
+					fmt.Print(letterHistory[i])
+					fmt.Print(" ")
+				}
+				fmt.Println("")
+				fmt.Println("")
 			}
-			tryRemain--
-			liveJose++
-			fmt.Println("")
-			fmt.Println("Mauvaise lettre.")
-			fmt.Println("")
-			fmt.Printf("Pour le moment le mot ressemble à ca -> ")
-			for i := 0; i < len(wordPatriallyReveal); i++ {
-				fmt.Print(wordPatriallyReveal[i])
+
+			// Vérifier si le mot est complètement deviné
+			wordFind := true
+			for _, letter := range wordPatriallyReveal {
+				if letter == "_" {
+					wordFind = false
+					break
+				}
 			}
-			fmt.Println("")
-			fmt.Print("L'historique de vos lettres est : ")
-			for i := 0; i <= len(letterHistory)-1; i++ {
-				fmt.Print(letterHistory[i])
-				fmt.Print(" ")
+			if wordFind == true {
+				fmt.Printf("\nVous avez deviné le mot !")
+				fmt.Println("")
+				fmt.Print("Vos propositions ont étaient : ")
+				for i := 0; i <= len(letterHistoryEnd)-1; i++ {
+					fmt.Print(letterHistoryEnd[i])
+					fmt.Print(" ")
+				}
+				fmt.Println("")
+				fmt.Println("")
+				return
 			}
-			fmt.Println("")
-			fmt.Println("")
 		}
 
-		// Vérifier si le mot est complètement deviné
-		wordFind := true
-		for _, letter := range wordPatriallyReveal {
-			if letter == "_" {
-				wordFind = false
-				break
-			}
-		}
-		if wordFind == true {
-			fmt.Printf("\nVous avez deviné le mot !")
-			fmt.Println("")
-			fmt.Print("Vos propositions ont étaient : ")
-			for i := 0; i <= len(letterHistoryEnd)-1; i++ {
-				fmt.Print(letterHistoryEnd[i])
-				fmt.Print(" ")
-			}
-			fmt.Println("")
-			fmt.Println("")
-			return
-		}
-	}
+		fmt.Print("\nVous n'avez plus de vie. Le mot était : ")
+		for i := 0; i < len(arrSelectWord); i++ {
+			fmt.Print(arrSelectWord[i])
 
-	fmt.Print("\nVous n'avez plus de vie. Le mot était : ")
-	for i := 0; i < len(arrSelectWord); i++ {
-		fmt.Print(arrSelectWord[i])
-
+		}
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Println(live[10])
+		fmt.Print("Vos propositions ont étaient : ")
+		for i := 0; i <= len(letterHistoryEnd)-1; i++ {
+			fmt.Print(letterHistoryEnd[i])
+			fmt.Print(" ")
+		}
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Println("Vous êtes pendu !")
 	}
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println(live[10])
-	fmt.Print("Vos propositions ont étaient : ")
-	for i := 0; i <= len(letterHistoryEnd)-1; i++ {
-		fmt.Print(letterHistoryEnd[i])
-		fmt.Print(" ")
-	}
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("Vous êtes pendu !")
 }
-
-// NORMALEMENT LE CODE FONCTIONNE MAIS SI TU VOIS UN SOUCIS PREND UNE PHOTO ET ESSAYE DE LE CORRIGER APRES JE M'EN OCCUPERAIS SI T'AS PAS TROUVE !!
-// BONNE CHANCE MAIS TU VAS VOIR C'EST PAS DUR A COMPRENDRE QUAND C'EST DEJA ECRIS ET LA LA MOITIE DU TRAVAIL EST DEJA FINIS NORMALMENT LE RESTE A L'AIR FACILE !!
-
-/* A MODIFIER QUE POUR LE SYSTEME DE NIVEAU SINON TU T'OCCUPES QUE DE CE QUI EST AU DESSUS (ON FERA UN COPIER COLLER DU CODE POUR LES AUTRES NIVEAU COMME CA SERA LA MEME CHOSE DE TOUTE FACON !)
-   	if difficulty == 2 {
-   		f, _ := os.Open("C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words2.txt")
-
-   		defer f.Close()
-
-   		scanner := bufio.NewScanner(f)
-   		scanner.Split(bufio.ScanWords)
-   		wordlist := []string{}
-   		for scanner.Scan() {
-   			wordlist = append(wordlist, scanner.Text())
-   		}
-   		indexword := rand.Intn(len(wordlist)) - 1
-   		if indexword == 0 {
-   			indexword += 1
-   		}
-   		selectword := wordlist[indexword]
-
-   		for i := 0; i <= len(selectword)-1; i++ {
-   			fmt.Print("_")
-   		}
-   	}
-
-   	if difficulty == 3 {
-   		f, _ := os.Open("C:\\Ytrack\\tls-challenge-go-23-24\\hangman-classic\\words3.txt")
-
-   		defer f.Close()
-
-   		scanner := bufio.NewScanner(f)
-   		scanner.Split(bufio.ScanWords)
-   		wordlist := []string{}
-   		for scanner.Scan() {
-   			wordlist = append(wordlist, scanner.Text())
-   		}
-   		indexword := rand.Intn(len(wordlist)) - 1
-   		if indexword == 0 {
-   			indexword += 1
-   		}
-   		selectword := wordlist[indexword]
-
-   		for i := 0; i <= len(selectword)-1; i++ {
-   			fmt.Print("_")
-   		}
-   	}
-
-   }
 */
