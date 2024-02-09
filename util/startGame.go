a7e26f4ed9f2b4b298756dc6cf66835dfaade3dc
carquentin <quentin.carrola@ynov.com>
2024-02-09 15:21:40 +0100

    Fix word, letter history

--- util/startGame.go
+++ util/startGame.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-func StartGame(choice string, wordPartiallyReveal []string, letterHistory []string, wordHistory []string) bool {
+func StartGame(choice string, wordPartiallyReveal []string, letterHistory []string, wordHistory []string) (bool, []string, []string) {
 	var (
 		choiceToLower        string
 		choiceToLowerStrings []string
@@ -33,7 +33,7 @@ func StartGame(choice string, wordPartiallyReveal []string, letterHistory []stri
 		}
 	} else {
 		goodInput = false
-		return goodInput
+		return goodInput, letterHistory, wordHistory
 	}
 
 	if goodInput == true {
@@ -45,5 +45,5 @@ func StartGame(choice string, wordPartiallyReveal []string, letterHistory []stri
 			wordHistory = append(wordHistory, choiceToLower) //Else, append the word to wordHistory
 		}
 	}
-	return goodInput
+	return goodInput, letterHistory, wordHistory
 }
